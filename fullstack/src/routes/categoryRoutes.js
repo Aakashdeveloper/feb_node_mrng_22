8200642758b13ab43e42b17bdbc86c774f02bf04
aakashdeveloper <ahanda205@gmail.com>
2022-02-07 09:53:48 +0530

    mongoconnect

--- fullstack/src/routes/categoryRoutes.js
+++ fullstack/src/routes/categoryRoutes.js
@@ -1,35 +1,27 @@
 let express = require('express');
 let categoryRoutes = express.Router();
+let mongodb = require('mongodb').MongoClient;
+const url = process.env.mongourl;
 
-
-var category = [
-    {
-        "id":1,
-        "category": "Fashion",
-        "thumb":"https://i.ibb.co/56VP0Fn/cloths.jpg"
-    },
-    {
-        "id":2,
-        "category":"Electronics",
-        "thumb":"https://i.ibb.co/pw5Wtdx/appliances.jpg"
-    },
-    {
-        "id":3,
-        "category":"Essentials",
-        "thumb":"https://i.ibb.co/0cw34xm/essentials.jpg"
-    },
-    {
-        "id":4,
-        "category": "Footwear",
-        "thumb":"https://i.ibb.co/r3SZq8S/footware.jpg"
-    }
-]
 
 function router(menu){
-
     categoryRoutes.route('/')
         .get(function(req,res){
-            res.render('category',{title:'Category',data:category,menu})
+            mongodb.connect(url,function(err,dc){
+                if(err){
+                    res.status(500).send('Error while connecting')
+                }else{
+                    let dbObj = dc.db('febnode');
+                    dbObj.collection('category').find().toArray(function(err,response){
+                        if(err){
+                            res.status(500).send('Error while fetching data')
+                        }else{
+                            res.render('category',{title:'Category',data:response,menu})
+                        }
+                    })
+                }
+            })
+            
         })
 
     categoryRoutes.route('/details')
