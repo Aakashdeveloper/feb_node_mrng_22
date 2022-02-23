d3aec41453b8b4a3eb98671fcf0d8abcbb244ee8
aakashdeveloper <ahanda205@gmail.com>
2022-02-23 09:50:37 +0530

    loginwithjwt

---
+++ loginwithjwt/controller/authController.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config');
+const User = require('../model/userSchema');
+
+router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.json());
+
+//get all users
+router.get('/users',(req,res) => {
+    User.find({},(err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+//register
+router.post('/register',(req,res) => {
+    //encrypt password
+    let hashpassword = bcrypt.hashSync(req.body.password,8);
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:hashpassword,
+        phone:req.body.phone,
+        role:req.body.role?req.body.role:'User',
+    },(err,data) => {
+        if(err) return res.status(500).send('Error While  Register');
+        res.status(200).send('Register Successful')
+    })
+})
+
+//login users
+router.post('/login', (req,res) => {
+    User.findOne({email:req.body.email},(err,user) => {
+        if(err) return res.status(500).send({auth:false,token:'Error While Login'})
+        if(!user) return res.status(200).send({auth:false,token:'No User Found Register First'})
+        else{
+            const passIsValid = bcrypt.compareSync(req.body.password, user.password)
+            if(!passIsValid) return res.status(200).send({auth:false,token:'Invalid Password'})
+            // in case pass is valid generate token and
+            let token = jwt.sign({id:user._id},config.secret,{expiresIn:86400})
+            return res.status(200).send({auth:true,token:token})
+        }
+    })
+})
+
+// userinfo
+router.get('/userinfo', (req, res) => {
+    let token = req.headers['x-access-token'];
+    if(!token) res.send({auth:false,token:'No Token Provided'})
+    //verfiy token
+    jwt.verify(token, config.secret,(err,user) => {
+        if(err) res.send({auth:false,token:'Invalid Token'})
+        User.findById(user.id,{password:0},(err,result) => {
+            res.send(result)
+        })
+    })
+})
+
+module.exports = router;
