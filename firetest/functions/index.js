2bbfc84adcdc72935112067ced52f7457f3ba3a8
aakashdeveloper <ahanda205@gmail.com>
2022-03-08 08:53:50 +0530

    display

--- firetest/functions/index.js
+++ firetest/functions/index.js
@@ -1,13 +1,9 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const express = require("express");
-const bodyParser = require("body-parser");
+
 
 admin.initializeApp(functions.config().firebase);
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 const db = admin.firestore();
 
 exports.addMessage = functions.https.onRequest(async (req, res) => {
